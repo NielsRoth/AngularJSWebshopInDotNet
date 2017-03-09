168d33f892ecb9d2e0b5c1161d751f9ed1a212c0
niels <niels.roth@gmx.net>
2017-03-09 12:23:50 +0100

    Eerste opzet

---
+++ AngularJSWebshopInDotNet/controllers/sportsStore.js
@@ -0,0 +1,10 @@
+angular.module("sportsStore")
+.controller("sportsStoreCtrl", function ($scope) {
+	$scope.data = {
+		products: [
+		{ name: "Prod1", description: "Prod1", category: "Cat1", price: 100 },
+		{ name: "Prod2", description: "Prod2", category: "Cat2", price: 110 },
+		{ name: "Prod3", description: "Prod3", category: "Cat1", price: 120 },
+		{ name: "Prod4", description: "Prod4", category: "Cat3", price: 130 }]
+	};
+});
